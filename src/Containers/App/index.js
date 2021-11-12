a2a20842bfb43500b5811c1cb6cc61bd2f96f4c3
Nimesh Shrestha <nimesh_03@yahoo.com>
2021-11-12 08:32:08 +0600

    RT : Updated STate and Props

--- src/Containers/App/index.js
+++ src/Containers/App/index.js
@@ -1,5 +1,6 @@
 import './App.scss';
 import React from "react";
+import FirstComponent from "../User/FirstComponent";
 
 class App extends React.Component {
   constructor(props) {
@@ -7,45 +8,76 @@ class App extends React.Component {
     this.state = {
       counters: 0,
       name: "default",
+      nameObj: {
+        firstName: "Mark",
+        lastName: "Wal",
+      },
+      nameListArr: ["Hello", "world"],
       showContainer: false,
     }
   }
-  handleChange = () => {
+  handleChange = (key, value) => {
     this.setState({
-      counters: this.state.counters + 1,
+      [key]: value,
     })
   }
-
-  handleOnChange = (value) => {
-    this.setState({
-      name: value,
-    })
-  }
-
   handleShowChange = () => {
     this.setState({
       showContainer: !this.state.showContainer,
     })
   }
+  handleUpdateField = (key, value) => {
+    this.setState({
+      nameObj: {
+        ...this.state.nameObj,
+        [key]: value
+      }
+    })
+  }
+  handleUpdateArrField = (index, value) => {
+    let updatedNameListArr = [...this.state.nameListArr];
+    updatedNameListArr[index] = value;
+    this.setState({
+      nameListArr: [...updatedNameListArr]
+    })
+  }
   render() {
+    const styleComponent = {
+      marginLeft: "10px",
+      marginRight: "10px"
+    }
+    const { nameListArr, counters, name } = this.state;
+    const { handleChange } = this;
     return (
       <div className="first-component" style={{ padding: "50px", border: "1px solid black" }}>
-        Parent Component {this.state.counters}
+        Parent Component {counters}
         <div>
-          <input value={this.state.name} onChange={(event) => this.handleOnChange(event.target.value)} />
-          <button onClick={this.handleChange} style={{ marginTop: "50px", backgroundColor: "#E67E22", color: "white", borderRadius: "5px" }}>
+          <label>Name State</label>
+          <input
+            style={styleComponent}
+            value={name}
+            onChange={(event) => this.handleChange("name", event.target.value)} />
+          {/* <label>First Name</label>
+          <input style={styleComponent} value={nameListArr[0]} onChange={(event) => this.handleUpdateArrField(0, event.target.value)} />
+          <label>Last Name</label>
+          <input style={styleComponent} value={nameListArr[1]} onChange={(event) => this.handleUpdateArrField(1, event.target.value)} /> */}
+          <button onClick={() => handleChange("counters", counters + 1)} style={{ marginTop: "50px", backgroundColor: "#E67E22", color: "white", borderRadius: "5px" }}>
             Click for increment
           </button>
-          <button onClick={this.handleShowChange} style={{ marginLeft: "10px", backgroundColor: "#E67E22", color: "white", borderRadius: "5px" }}>
+          {/* <button onClick={this.handleShowChange} style={{ marginLeft: "10px", backgroundColor: "#E67E22", color: "white", borderRadius: "5px" }}>
             Click to show Container
-          </button>
+          </button> */}
         </div>
 
-        {this.state.showContainer &&
+        {/* {this.state.showContainer &&
           <div style={{ with: "200px", padding: "50px" }}>
             Show container on condition
           </div>
         }
+        <FirstComponent
+          counters={this.state.counters}
+          handleChange={this.handleChange}
+        /> */}
       </div>
     );
   }
--- src/Containers/User/FirstComponent.js
+++ src/Containers/User/FirstComponent.js
@@ -1,28 +1,20 @@
 import React from "react";
-import './App.scss';
 
 class FirstComponent extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      counters: 0,
-    }
-  }
-  componentDidMount() {
-    document.title = "CHange from first component";
-  }
-  handleChange = () => {
-    this.setState({
-      counters: this.state.counters + 1,
-    })
   }
   render() {
+    const {
+      counters,
+      handleChange,
+    } = this.props;
     return (
       <div className="first-component" style={{ padding: "50px", border: "1px solid black" }}>
-        First Component {this.props.counters}
+        First Component counter state : {counters}
         <div>
-          <button onClick={this.props.handleChange} style={{ marginTop: "50px", backgroundColor: "#E67E22", color: "white", borderRadius: "5px" }}>
-            Click for increment
+          <button onClick={() => handleChange(100)} style={{ marginTop: "50px", backgroundColor: "#E67E22", color: "white", borderRadius: "5px" }}>
+            Click for increment 100
             </button>
         </div>
       </div>
